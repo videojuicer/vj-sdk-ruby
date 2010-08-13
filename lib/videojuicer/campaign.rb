0876a572898cf5c4fe7009a517d163c0f77074ac
Ted Han <ted@knowtheory.net>
2010-08-13 10:13:11 -0400

    Modifying campaigns to apply to an insert domain as opposed to a target type.

--- lib/videojuicer/campaign.rb
+++ lib/videojuicer/campaign.rb
@@ -5,8 +5,8 @@ module Videojuicer
     
     property :name,         String
     property :user_id,      Integer
-    property :target_type,  String
-    property :target_id,    Integer
+    property :insert_domain,  String
+    property :presentation_id,    Integer
     property :disclosure,   String
     property :created_at,   DateTime
     property :updated_at,   DateTime
@@ -14,7 +14,7 @@ module Videojuicer
     belongs_to :user, :class=>Videojuicer::User
     
     attr_accessor :criteria
-    attr_accessor :targets
+    attr_accessor :presentations
     attr_accessor :inserts
   end
 end
